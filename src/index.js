e099fd22d475099d34414367c2d0d47c6ccb6cf3
dennis00095 <dennis.mwongela@student.moringaschool.com>
2022-08-29 22:06:40 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  
-}
+    const form = document.getElementsByTagName("form")[0];
+    const h4 = document.querySelector("section > h4");
+    const p = document.querySelector("section > p");
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const index = form.elements["searchByID"].value;
+        fetch("http://localhost:3000/movies")
+            .then((response) => response.json())
+            .then((data) => {
+                h4.textContent = data[index]["title"];
+                p.textContent = data[index]["summary"];
+            });
+    });
+};
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
